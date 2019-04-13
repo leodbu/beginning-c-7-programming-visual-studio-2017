8cb65934178731d85707c6bd997384836f239a69
leodbu <44317694+leodbu@users.noreply.github.com>
2019-04-13 13:03:18 +0700

    learning C# with VS 2017

---
+++ chapter05/Ch05Ex04/Ch05Ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = {"Todd Anthony", "Kevin Holton", "Shane Laigle"};
+            int i;
+            WriteLine($"Here are {friendNames.Length} of my friends:");
+            for (i=0; i < friendNames.Length; i++)
+            {
+                WriteLine(friendNames[i]);
+            }
+
+            ReadKey();
+
+            foreach (string friendName in friendNames)
+            {
+                WriteLine(friendName);
+            }
+
+        }
+    }
+}
