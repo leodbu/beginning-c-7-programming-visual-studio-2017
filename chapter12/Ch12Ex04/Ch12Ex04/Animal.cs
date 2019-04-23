bc4b200a164e6c9239af40cf3f8c7595f5549959
leodbu <44317694+leodbu@users.noreply.github.com>
2019-04-23 22:10:50 +0700

    Learn C# with VS 2017 E at chapter 12 -> Beginning C# 7 with VS 2017

---
+++ chapter12/Ch12Ex04/Ch12Ex04/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch12Ex04
+{
+    /// <summary>
+    /// Class Animal is 
+    /// </summary>
+    public abstract class Animal
+    {
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        public Animal() => name = "The Animal with no name";
+        public Animal(String newName) => name = newName;
+        public void Feed() => WriteLine($"{name} has beed fed.");
+
+        public abstract void MakeANoise();
+    }
+}
