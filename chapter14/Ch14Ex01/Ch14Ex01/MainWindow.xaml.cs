9f64276c20dde585e2561f962da5127d99b0f4da
leodbu <44317694+leodbu@users.noreply.github.com>
2019-05-02 13:55:07 +0700

    Learn C# with VS 2017 E - chapter 14

---
+++ chapter14/Ch14Ex01/Ch14Ex01/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch14Ex01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRotated_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotated button handler, bubbling up");
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, bubbling up");
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, bubbling up");
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, tunneling up");
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, tunneling down");
+           //e.Handled = true;
+        }
+
+        private void ButtonRotated_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotated button handler, tunneling up");
+        }
+    }
+}
